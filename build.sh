b16f8422cebf3b925eb0d2440c6229b26e82fd1f
György Andrasek <jurily@gmail.com>
2014-09-28 22:47:00 +0200

    remove C files and build script, the tests are good enough now

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-mkdir -p build
-gcc   -fPIC -Ofast -march=native -std=c99 -o build/libxxhash-gcc.a   -c cbits/xxhash.c
-clang -fPIC -Ofast -march=native -std=c99 -o build/libxxhash-clang.a -c cbits/xxhash.c
-
-rustc -Lbuild -C no-vectorize-slp --cfg gcc --opt-level=3 --out-dir=build lib.rs --test
