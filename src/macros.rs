ff16980e763cba5cb1b75b05604bc503ba3a41f1
Jake Goulding <jake.goulding@gmail.com>
2015-01-02 12:00:44 -0500

    Remove unused `use Int`

--- src/macros.rs
+++ src/macros.rs
@@ -6,7 +6,6 @@
 macro_rules! read_ptr(($p:ident, $rem:ident, $size:ty) => ({
     #[allow(unused_assignments)]
     use core::mem;
-    use core::num::Int;
     let mut dp: *const $size = mem::transmute($p);
     let data: $size = *dp;
     dp = dp.offset(1);
